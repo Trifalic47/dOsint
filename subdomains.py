597f437482e94308487ff65f88dfdf3a1c6b27d2
trifalicprime <trifalicprime@gmail.com>
2026-01-25 00:41:41 +0530

    subdomains.py v1.0

--- subdomains.py
+++ subdomains.py
@@ -4,25 +4,23 @@ import requests
 
 init(autoreset=True)
 
-if __name__ == "__main__":
-    url = "https://crt.sh/?q=%25.tesla.com&output=json"
+
+def scanSubdomain(domainName):
+    url = f"https://crt.sh/?q=%.{domainName}&output=json"
     response = requests.get(url)
     data = response.json()
 
-    # print(data)
-
-    # Fetching subdomain from the JSON
-    subdomain = set()  # This will automatically remove the duplicate
+    subdomains = set()
     for entry in data:
-        # Get the name_value from the entry
         name = entry["name_value"]
-
-        # Split from \n to prevent new line characters
         lines = name.split("\n")
-        # print(lines)
-
         for line in lines:
             clean_name = line.replace("*.", "").strip()
-            subdomain.add(clean_name)
-    for sub in sorted(subdomain):
-        print(f"[+]Found:{Fore.BLUE}{sub}")
+            subdomains.add(clean_name)
+    for sub in sorted(subdomains):
+        print(f"{Fore.GREEN}[+]Found: {Fore.BLUE}{sub}")
+
+
+if __name__ == "__main__":
+    domain = input("Enter domain name:")
+    scanSubdomain(domain)
